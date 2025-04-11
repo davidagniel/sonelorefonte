616b3f37828a80a9bebbab4c09ef0dc133ec3492
david agniel <71008274+davidagniel@users.noreply.github.com>
2025-04-11 14:56:02 +0200

    accueil

---
+++ Sonelo/dashboard/accueil.php
@@ -0,0 +1,40 @@
+<div class="col-md-12 col-lg-12">
+<div class="card">
+
+<div class="card-header">
+                    <h2 class="card-title" style="display:flex;"> <div style="width:90%;display:flex;align-items: left; justify-content: left;">Mes certifications </div> </h2>
+                    </div>
+                    <div class="card-body">
+                   <div class="table-responsive">
+                    <table class='table table-striped mt-6' style="text-align:center;">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Certification</th>
+                        <th>Type</th>
+                        <th>Date d'obtention</th>
+                        <th>
+                            Obligation de formation
+                            </th>
+                            <th></th>
+                        <th> Fin de cycle</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><img src="image/ss4.svg" width="50" height="50" alt="ss4"></td>
+                        <td>Sous amiante 4</td>
+                        <td>initiale</td>
+                        <td>10/10/2021</td>
+                        <td class="text-white" style="background-color:#fd7e14;">10/10/2025</td>
+                        <td><a href="#"><span data-feather="search"></span></a></td>
+                        <td>09/10/2028</td>
+                        <td><a href="#"> <span data-feather="layers" ></span>
+                        </a></td>
+                    </tr>
+                </tbody>
+                </table>
+</div>
+</div>
+</div>
