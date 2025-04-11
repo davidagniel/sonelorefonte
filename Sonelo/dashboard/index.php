41f1fe5a662bd00d7b347bd832ee93bafd013110
david agniel <71008274+davidagniel@users.noreply.github.com>
2025-04-11 17:24:58 +0200

    accueil

--- Sonelo/dashboard/index.php
+++ Sonelo/dashboard/index.php
@@ -226,7 +226,7 @@
   </nav>
 
   <!-- Main content -->
-  <main class="flex-grow-1 p-4">
+  <main class="flex-grow-1 p-2">
   <div class="container-fluid">
 <div class="d-none d-lg-block bg-secondary ">
 <div class="row align-items-center">
