0f5f3db35b9428c8294dc8971591fc6a1061ba27
david agniel <71008274+davidagniel@users.noreply.github.com>
2025-04-11 12:59:44 +0200

    logo

--- Sonelo/dashboard/index.php
+++ Sonelo/dashboard/index.php
@@ -67,12 +67,23 @@
       </button>
   
       <span class="navbar-brand mb-1 h1 ms-auto"> <img src="image/AlerteCertif-accueil.png" width="150" height="50" alt="logo"/></span>
-      <div class="row">
-        <div class="ms-auto">
-      <span class="navbar-brand mb-0 h1"><img src="image/image_societe.png" width="150" height="70" alt="societe"></span>
-      <span class="navbar-brand mb-0 h1"><a href="#"><img src="image/image_profil.png" width="50" height="50" alt="societe"></a></span>
-      </div>
+      <div class="row align-items-center">
+  <!-- Premier span centré -->
+  <div class="col-12 text-center">
+    <span class="navbar-brand mb-0 h1">
+      <img src="image/image_societe.png" width="150" height="70" alt="societe">
+    </span>
   </div>
+
+  <!-- Deuxième span à droite -->
+  <div class="col-12 text-end">
+    <span class="navbar-brand mb-0 h1">
+      <a href="#">
+        <img src="image/image_profil.png" width="50" height="50" alt="profil">
+      </a>
+    </span>
+  </div>
+</div>
     </div>
   </nav>
 
