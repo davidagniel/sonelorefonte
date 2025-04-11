39daaf202ba7e77cba0be77cca760d2ca8eeaac1
david agniel <71008274+davidagniel@users.noreply.github.com>
2025-04-11 15:23:08 +0200

    accueil

--- Sonelo/dashboard/accueil.php
+++ Sonelo/dashboard/accueil.php
@@ -1,41 +1,40 @@
-<div class="col-md-12 p-4">
-<div class="card">
+<div class="col-md-12 p-4" style="max-width: 768px; margin: auto;">
+  <div class="card">
+    
+    <div class="card-header">
+      <h2 class="card-title d-flex justify-content-start">Mes certifications</h2>
+    </div>
 
-<div class="card-header col-md-12">
-                    <h2 class="card-title" style="display:flex;"> <div style="width:90%;display:flex;align-items: left; justify-content: left;">Mes certifications </div> </h2>
-                    </div>
-                    <div class="card-body">
-                   <div class="table-responsive">
-                    <table class='table table-striped mt-5' style="text-align:center;">
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Certification</th>
-                        <th>Type</th>
-                        <th>Date d'obtention</th>
-                        <th>
-                            Obligation de formation
-                            </th>
-                            <th></th>
-                        <th> Fin de cycle</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td><img src="image/ss4.svg" width="50" height="50" alt="ss4"></td>
-                        <td>Sous amiante 4</td>
-                        <td>initiale</td>
-                        <td>10/10/2021</td>
-                        <td class="text-white" style="background-color:#fd7e14;">10/10/2025</td>
-                        <td><a href="#" class="text-dark"><span data-feather="search"></span></a></td>
-                        <td>09/10/2028</td>
-                        <td><a href="#" class="text-dark"> <span data-feather="layers" ></span>
-                        </a></td>
-                    </tr>
-                </tbody>
-                </table>
-</div>
-</div>
-</div>
+    <div class="card-body">
+      <div class="table-responsive">
+        <table class="table table-striped mt-5 text-center">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Certification</th>
+              <th>Type</th>
+              <th>Date d'obtention</th>
+              <th>Obligation de formation</th>
+              <th></th>
+              <th>Fin de cycle</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td><img src="image/ss4.svg" width="50" height="50" alt="ss4"></td>
+              <td>Sous amiante 4</td>
+              <td>initiale</td>
+              <td>10/10/2021</td>
+              <td class="text-white" style="background-color:#fd7e14;">10/10/2025</td>
+              <td><a href="#" class="text-dark"><span data-feather="search"></span></a></td>
+              <td>09/10/2028</td>
+              <td><a href="#" class="text-dark"><span data-feather="layers"></span></a></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+  </div>
 </div>
