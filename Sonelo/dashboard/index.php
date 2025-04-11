f9735d8b88b0daa030cc43b475f1eb5518ef0db9
david agniel <71008274+davidagniel@users.noreply.github.com>
2025-04-11 13:04:55 +0200

    logo

--- Sonelo/dashboard/index.php
+++ Sonelo/dashboard/index.php
@@ -77,11 +77,11 @@
 
   <!-- Deuxième span à droite -->
   <div class="col-6 d-flex justify-content-end align-items-center">
-  <span class="mb-0 h1">
+  
     <a href="#">
       <img src="image/image_profil.png" width="50" height="50" alt="profil">
     </a>
-  </span>
+  
 </div>
 </div>
     </div>
