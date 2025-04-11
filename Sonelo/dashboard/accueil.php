c0fa2e75d9fc11d7c3812fed8c1c6a05757e3e1f
david agniel <71008274+davidagniel@users.noreply.github.com>
2025-04-11 18:13:35 +0200

    accueil

--- Sonelo/dashboard/accueil.php
+++ Sonelo/dashboard/accueil.php
@@ -1,3 +1,4 @@
+  <div class="p-2">
   <div class="card">
 
     <div class="card-header">
@@ -35,4 +36,104 @@
       </div>
     </div>
 </div>
+</div>
 
+<div style="display:flex; p-2">
+                        <div style="width: 25%;display:flex;">
+                            <div style="
+                            display:flex;align-items: center; justify-content: center;
+                            border: 2px solid #6ce364;
+                            border-radius: 2px;
+                           
+                                    width: 50px;
+                                    height:10px;
+                            box-shadow: 0 4px 8px #6ce364;
+                            background-color:#6ce364;">   
+                                
+                            </div>
+                            <div style="width:50%;display:flex;align-items: center; justify-content: center;font-weight: bold;">  A jour </div>
+                        </div>
+                        <div style="width: 25%;display:flex;">
+                                            <div style="
+                                            display:flex;align-items: center; justify-content: center;
+                                            border: 2px solid #6ec2e6;
+                                    border-radius: 2px;
+                                
+                                    width: 50px;
+                                    height:10px;
+                                    box-shadow: 0 4px 8px #6ec2e6;
+                                    background-color:#6ec2e6;">   
+                                            
+                                            </div><div style="width:50%;display:flex;align-items: center; justify-content: center; font-weight: bold;"> Réalisé</div>
+                        </div>
+                        <div style="width: 25%;display:flex;">
+                                        <div style="
+                                        display:flex;align-items: center; justify-content: center;
+                                        border: 2px solid #e0823a;
+                                    border-radius: 2px;
+                                    
+                                   
+                                   width: 50px;
+                                    height:10px;
+                                    box-shadow: 0 4px 8px #e0823a;
+                                    background-color:#e0823a">   
+                                        
+                                            </div><div style="width:50%;display:flex;align-items: center; justify-content: center; font-weight: bold;">  A échéance </div>
+                        </div>
+                        <div style="width: 25%;display:flex;">
+                                            <div style="
+                                            display:flex;align-items: center; justify-content: center;
+                                            border: 2px solid #c90e0e;
+                                    border-radius: 2px;
+                                
+                                  
+                                    width: 50px;
+                                    height:10px;
+                                    box-shadow: 0 4px 8px #c90e0e;
+                                    background-color:#c90e0e;">   
+                                            
+                                            </div>
+                    <div style="width:70%;display:flex;align-items: center; justify-content: center; font-weight: bold;"> Echéance dépassée</div>
+                        </div>
+                    </div>
+
+<div class="p-2">
+  
+<div class="card">
+
+<div class="card-header">
+  <h2 class="card-title d-flex justify-content-start">Mes attestations</h2>
+</div>
+
+<div class="card-body">
+  <div class="table-responsive">
+    <table class="table table-striped text-center">
+      <thead>
+        <tr>
+          <th></th>
+          <th>attestation</th>
+          <th>Type</th>
+          <th>Date d'obtention</th>
+          <th>Obligation de formation</th>
+          <th></th>
+          <th>Fin de cycle</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td><img src="image/audit.svg" width="50" height="50" alt="ss4"></td>
+          <td>audit</td>
+          <td>initiale</td>
+          <td>10/10/2021</td>
+          <td class="text-white" style="background-color:#fd7e14;">10/10/2025</td>
+          <td><a href="#" class="text-dark"><span data-feather="search"></span></a></td>
+          <td>09/10/2028</td>
+          <td><a href="#" class="text-dark"><span data-feather="layers"></span></a></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
+</div>
+</div>
