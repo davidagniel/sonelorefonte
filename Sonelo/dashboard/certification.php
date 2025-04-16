70bff963c1c9e702c72be8ceaefc68baf87c6a76
david agniel <71008274+davidagniel@users.noreply.github.com>
2025-04-16 11:28:12 +0200

    certif

--- Sonelo/dashboard/certification.php
+++ Sonelo/dashboard/certification.php
@@ -1,5 +1,6 @@
 <div class="row">
 <div class="d-none d-lg-block d-flex flex-row">
+    <div class="row">
 <div class="col-lg-3">
 <select>
     <option value="">Certification</option>
@@ -26,6 +27,8 @@
 </div>
 </div>
 </div>
+</div>
+
 <div class="row">
     <div class="col-lg-6">
     <div class="card p-4 ">
