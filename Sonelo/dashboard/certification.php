2d51d8be1278f1408b07ad04440e48b4a3da7624
david agniel <71008274+davidagniel@users.noreply.github.com>
2025-04-16 10:16:24 +0200

    certif

--- Sonelo/dashboard/certification.php
+++ Sonelo/dashboard/certification.php
@@ -33,7 +33,7 @@
             </tr>
           <tr>
                          <td colspan=3><a href="#" class="text-dark"><span data-feather="layers"></span></a></td>
-            </td>
+            
         </tr>
 <tr>
     <td colspan=3 class="d-flex flex-column">
