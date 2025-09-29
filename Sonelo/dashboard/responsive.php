10de6e8ef51a4d8b652cd070d6c53eee4d9b3a04
davidagniel <agnieldavid@gmail.com>
2025-09-29 15:25:03 +0200

    responsive

--- Sonelo/dashboard/responsive.php
+++ Sonelo/dashboard/responsive.php
@@ -12,12 +12,13 @@
   <!-- Navbar -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">MonSite</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
               data-bs-target="#navbarNav" aria-controls="navbarNav" 
               aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
-      </button>
+      </button>  
+    <a class="navbar-brand" href="#">MonSite</a>
+      
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ms-auto">
           <li class="nav-item"><a class="nav-link active" href="#">Accueil</a></li>
